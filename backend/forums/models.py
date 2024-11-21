a39b39af328afe95f5093795ff6a36ed47493cef
= <=>
2024-11-21 19:15:52 +0300

    create forums and allow users reply to forums

--- backend/forums/models.py
+++ backend/forums/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from lesson.custom import UUID_field
-
+from lesson.models import CustomUser
 
 class NewsArticle(models.Model):
     """
@@ -16,6 +16,40 @@ class NewsArticle(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Forum(models.Model):
+    id = UUID_field()
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class ForumThread(models.Model):
+    id = UUID_field()
+    forum = models.ForeignKey(
+        Forum, on_delete=models.CASCADE, related_name='threads'
+    )
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="threads")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.title
+
+class ForumReply(models.Model):
+    id = UUID_field()
+    thread = models.ForeignKey(ForumThread, on_delete=models.CASCADE, related_name="replies")
+    content = models.TextField()
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="replies")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Reply by {self.created_by.username} on {self.thread.title}"
 
 
     
